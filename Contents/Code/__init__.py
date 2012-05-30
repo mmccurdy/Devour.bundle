080035833f23f2bd8a2dbcd4d517bfef24833604
sander1 <sander@plexapp.com>
2012-05-30 03:08:39 +0200

    Add cache and UA header; update code layout

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -5,85 +5,87 @@ ICON = 'icon-default.png'
 SEARCH = 'icon-search.png'
 
 ###################################################################################################
-
 def Start():
-  Plugin.AddPrefixHandler('/video/devour', MainMenu, TITLE, ICON, ART)
-  Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
 
-  ObjectContainer.title1 = TITLE
-  ObjectContainer.view_group = 'List'
-  ObjectContainer.art = R(ART)
+	Plugin.AddPrefixHandler('/video/devour', MainMenu, TITLE, ICON, ART)
+	Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
 
-  DirectoryObject.thumb = R(ICON)
-  DirectoryObject.art = R(ART)
-  VideoClipObject.thumb = R(ICON)
-  VideoClipObject.art = R(ART)
+	ObjectContainer.title1 = TITLE
+	ObjectContainer.view_group = 'List'
+	ObjectContainer.art = R(ART)
+
+	DirectoryObject.thumb = R(ICON)
+	DirectoryObject.art = R(ART)
+	VideoClipObject.thumb = R(ICON)
+	VideoClipObject.art = R(ART)
+
+	HTTP.CacheTime = CACHE_1DAY
+	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:12.0) Gecko/20100101 Firefox/12.0'
 
 ###################################################################################################
-
 def MainMenu():
-  oc = ObjectContainer()
 
-  oc.add(DirectoryObject(key=Callback(LatestList), title="Latest Videos"))
-  oc.add(SearchDirectoryObject(identifier="com.plexapp.plugins.devour", title="Search for Videos", prompt="Search Devour for...", thumb=R(SEARCH), art=R(ART)))
+	oc = ObjectContainer()
 
-  return oc
+	oc.add(DirectoryObject(key=Callback(LatestList), title="Latest Videos"))
+	oc.add(SearchDirectoryObject(identifier="com.plexapp.plugins.devour", title="Search for Videos", prompt="Search Devour for...", thumb=R(SEARCH), art=R(ART)))
+
+	return oc
 
 ###################################################################################################
-
 def LatestList(page=1):
-  oc = ObjectContainer()
 
-  result = {}
+	oc = ObjectContainer()
 
-  @parallelize
-  def GetVideos():
+	result = {}
 
-    url = DEVOUR_URL
-    if page > 1:
-      url = '%s%d/' % (url, page)
+	@parallelize
+	def GetVideos():
 
-    html = HTML.ElementFromURL(url)
-    videos = html.xpath('//div[starts-with(@class, "orko")]')
+		url = DEVOUR_URL
+		if page > 1:
+			url = '%s%d/' % (url, page)
 
-    for num in range(len(videos)):
-      video = videos[num]
+		html = HTML.ElementFromURL(url)
+		videos = html.xpath('//div[starts-with(@class, "orko")]')
 
-      @task
-      def GetVideo(num = num, result = result, video = video):
-        try:
-          devour_url = video.xpath('./a')[0].get('href')
-          result[num] = DevourScrape(devour_url)
-        except:
-          Log("Couldn't add")
-          pass
+		for num in range(len(videos)):
+			video = videos[num]
 
-  keys = result.keys()
-  keys.sort()
+			@task
+			def GetVideo(num = num, result = result, video = video):
+				try:
+					devour_url = video.xpath('./a')[0].get('href')
+					result[num] = DevourScrape(devour_url)
+				except:
+					Log("Couldn't add video from %s" % devour_url)
+					pass
 
-  for key in keys:
-    oc.add(result[key])
+	keys = result.keys()
+	keys.sort()
 
-  oc.add(DirectoryObject(key=Callback(LatestList, page=page+1), title="More Videos..."))
-  return oc
+	for key in keys:
+		oc.add(result[key])
+
+	oc.add(DirectoryObject(key=Callback(LatestList, page=page+1), title="More Videos..."))
+
+	return oc
 
 ####################################################################################################
-#
 # DevourScrape takes a Devour video page URL and returns a well-formed VideoClipObject
-#
 
 def DevourScrape(devour_url):
 
-  devour_html = HTML.ElementFromURL(devour_url)
-  url = devour_html.xpath('//iframe')[0].get('src')
-  video = URLService.MetadataObjectForURL(url)
+	devour_html = HTML.ElementFromURL(devour_url)
+	url = devour_html.xpath('//iframe')[0].get('src')
+	video = URLService.MetadataObjectForURL(url)
 
-  # Use the Devour-provided title, description vs. the ones assocaited with the underlying clips.
-  video.title = devour_html.xpath('//div[@id="left"]/h1//text()')[0]
-  try:
-    description = devour_html.xpath('//div[@id="left"]/p//text()')
-    video.summary = ''.join(description)
-  except:
-    pass
+	# Use the Devour-provided title, description vs. the ones assocaited with the underlying clips.
+	video.title = devour_html.xpath('//div[@id="left"]/h1//text()')[0]
+	try:
+		description = devour_html.xpath('//div[@id="left"]/p//text()')
+		video.summary = ''.join(description)
+	except:
+		pass
 
-  return video
+	return video
